f799ca6404896f4ed9b6a6f9673db267414d4e3b
Cody Petrick <codyislucky@gmail.com>
2021-11-18 13:43:14 -0800

    Update etainLove.java

--- TwitchChatCommandAPIs/src/main/java/com/codypetrick/TwitchChatCommandAPIs/etainLove.java
+++ TwitchChatCommandAPIs/src/main/java/com/codypetrick/TwitchChatCommandAPIs/etainLove.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
+/*Custom command created by Cody Petrick for twitch.tv/etain
+Has an x% chance to output a secret "It was me, Dio" message.
+Otherwise, outputs a random love percentage between the user and a given target
+ */
+
 @RestController
 public class etainLove {
 
-    @GetMapping("getEtainLove/{user}/{target}")
+    @GetMapping("Twitch/etainLoveCommand/{user}/{target}")
     public String loveResponse(@PathVariable("user") String user, @PathVariable("target") String target){
         Random random = new Random();
         int messageChance = (random.nextInt(100) + 1); //random number for the secret message
@@ -23,11 +28,24 @@ public class etainLove {
             return (user + " is " + lovePercent + " in love with " + target + " etainOhMy");
         }
 
-
-
-
-
     }
+
+//    @GetMapping("Twitch/etainLoveCommand/{user}")
+//    public String loveResponse(@PathVariable("user") String user){
+//        Random random = new Random();
+//        int messageChance = (random.nextInt(100) + 1); //random number for the secret message
+//        int percentChance = 5; //percent chance out of 100 of the secret message occurring
+//
+//        if(messageChance <= percentChance){
+//            return ("!zz You thought you were in love with yourself but no, it was me, Cody!");
+//        }
+//        else{
+//            int lovePercent = (random.nextInt(100) + 1);
+//            return (user + " is " + lovePercent + " in love with themself etainOhMy");
+//        }
+//
+//    }
+
 
 
 }
