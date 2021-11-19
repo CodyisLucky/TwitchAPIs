d6a2b43cd760b040ce283ebc17d0a9e76194666b
Cody Petrick <codyislucky@gmail.com>
2021-11-19 10:52:52 -0800

    Create CowsAndBulls.java

---
+++ src/main/java/com/codypetrick/TwitchChatCommandAPIs/CowsAndBulls.java
@@ -0,0 +1,20 @@
+package com.codypetrick.TwitchChatCommandAPIs;
+
+/*
+To Do List:
+Write the initialize, guess, and force end command methods
+Figure out if a linked list will work for multiple channels or if a SQL server is needed
+if SQL server is needed, figure out how to implement that on Heroku
+ */
+
+
+public class CowsAndBulls {
+
+    private static boolean isActiveGame = false;
+    private static byte firstNum;
+    private static byte secondNum;
+    private static byte thirdNum;
+
+    public CowsAndBulls() {
+    }
+}
